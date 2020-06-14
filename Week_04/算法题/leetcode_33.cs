8b49b4002c17bc42c36db25e89196b36e7a2cc27
BeGemini <cutetoofat@outlook.com>
2020-06-15 00:01:09 +0800

    homework on week04

---
+++ Week_04/算法题/leetcode_33.cs
@@ -0,0 +1,25 @@
+public class Solution
+{
+    public int mySqrt(int x)
+    {
+        if(x == 0 || x == 1)
+        {
+            return x ;
+        }
+        long left = 1;
+        long right = x;
+        while(left <= right)
+        {
+            mid = left + (right - left) / 2;
+            if ( mid * mid > x)
+            {
+                right = mid - 1;
+            }
+            else
+            {
+                left = mid + 1;
+            }
+        }
+        return (int)right;
+    }
+}
